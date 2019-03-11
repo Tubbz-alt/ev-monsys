8cb7d1ef3a0b04579cf01b81514ed13f9447c7eb
Oki Marzuqi <33563037+omodoong@users.noreply.github.com>
2019-03-11 19:04:49 +0700

    Update install-nodejs.sh

--- _control/install-nodejs.sh
+++ _control/install-nodejs.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -e
 cd /usr/src &
-sudo wget http://nodejs.org/dist/v10.7.0/node-v10.7.0.tar.gz
-sudo tar xvzf node-v10.7.0.tar.gz
+sudo wget http://nodejs.org/dist/v11.9.0/node-v11.9.0.tar.gz
+sudo tar xvzf node-v11.9.0.tar.gz
 
-cd node-v10.7.0.tar.gz &
+cd node-v11.9.0.tar.gz &
 sudo ./configure
 sudo make
 sudo make install
